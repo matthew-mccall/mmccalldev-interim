474774bb31fa3b3e904bbc79ab3f8bcac0e8bbb6
Matthew McCall <matthew+github@mmccall.dev>
2023-06-25 14:20:12 -0400

    Add placeholder resume page

---
+++ src/app/resume/layout.tsx
@@ -0,0 +1,16 @@
+import NavigationBar from "@mmccalldev/components/NavigationBar";
+import React from "react";
+import Footer from "@mmccalldev/components/Footer";
+
+export default function Page({ children }: { children: React.ReactNode }) {
+
+    return (
+            <div className={"min-vh-100 d-flex flex-column"}>
+                <NavigationBar position={"fixed"} />
+                <div className={"flex-grow-1 d-flex flex-column"}>
+                    {children}
+                </div>
+                <Footer/>
+            </div>
+    )
+}
---
+++ src/app/resume/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+        <div className={"bg-body flex-grow-1 d-flex align-items-center"}>
+            <div className={"container"}>
+                <h1>Coming Soon!</h1>
+            </div>
+        </div>
+    )
+}
