ab662f9ee4ac80528b37f4fe26c1aca7875c28b4
Matthew McCall <matthew+github@mmccall.dev>
2023-06-25 13:54:46 -0400

    Add Gallery page

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,25 @@
+import {Content} from "@mmccalldev/lib/Content";
+import GetUnsplashContent from "@mmccalldev/lib/UnsplashContent";
+import ContentGrid from "@mmccalldev/components/ContentGrid";
+
+export default async function Page() {
+
+    const unsplashContent: Content[] =
+        (await Promise.all(await GetUnsplashContent()))
+        .sort((a, b) => {
+            return (new Date(b.date)).getTime() - (new Date(a.date)).getTime();
+        });
+
+    return (
+        <>
+            <div className={"py-5"}>
+                <div className={"container text-light"}>
+                    <h1 className={"display-3 fw-semibold"}>Gallery</h1>
+                </div>
+            </div>
+            <div className={"bg-body py-5 flex-grow-1"}>
+                <ContentGrid content={unsplashContent} maxColumns={3} />
+            </div>
+        </>
+    )
+}
